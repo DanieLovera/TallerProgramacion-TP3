3a27119544f06b96b389d2abb723cf1e882324d8
DanieLovera <daanilo2497@gmail.com>
2021-05-23 19:22:10 -0300

    Creo la clase ErrnoException para lanzar excepciones de tipo errno.
    La clase tiene un unico metodo que da la descripcion del error ocurrido

---
+++ common_ErrnoException.cpp
@@ -0,0 +1,7 @@
+#include "common_ErrnoException.h"
+
+ErrnoException::ErrnoException(int _errno) : _errno(_errno) { }
+
+const char* ErrnoException::what() const noexcept {
+    return strerror(_errno);
+}
